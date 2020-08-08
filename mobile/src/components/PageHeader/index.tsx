8a69ada2dd8dbb0aa28b28177355450d54a42dcb
Eduardo Balistieri <dudu21500@hotmail.com>
2020-08-07 23:41:23 -0300

    Dia 3 - Finalizando app mobile

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { View, Image, Text } from 'react-native'
 import { BorderlessButton } from 'react-native-gesture-handler'
 
@@ -9,11 +9,12 @@ import { useNavigation } from '@react-navigation/native'
 
 
 interface PageHeaderProps {
-    title: string
+    title: string,
+    headerRight?: ReactNode
 }
 
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
 
 
     const { navigate } = useNavigation()
@@ -33,10 +34,15 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
                 <Image source={logoImg} resizeMode='contain' />
             </View>
 
-            <Text style={styles.title}>
-                {title}
-            </Text>
+            <View style={styles.header}>
+                <Text style={styles.title}>
+                    {title}
+                </Text>
+                
+                {headerRight}
+            </View>
 
+            {children}
         </View>
     )
 }
